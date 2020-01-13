e6d9b1d1b1ffdc717a402c53e4f2f72c1a217f51
japoco <jacopo@airmail.cc>
2020-01-13 22:58:03 +0100

    added cancancn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_092943) do
+ActiveRecord::Schema.define(version: 2020_01_13_182752) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: ""
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_01_12_092943) do
     t.string "username"
     t.string "provider"
     t.string "uid"
+    t.boolean "admin", default: false
+    t.boolean "janitor", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
