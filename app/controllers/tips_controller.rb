6bf3da0b74561ee4f4d234e4e0e350781e05480a
japoco <jacopo@airmail.cc>
2020-01-15 17:43:04 +0100

    basic tip show and new

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,11 +1,27 @@
 class TipsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @tip = Tip.new
   end
 
   def create
+    @user = current_user
+    @tip = @user.tips.new(tip_params)
+
+    if @tip.save
+      redirect_to @tip
+    else
+      render 'new'
+    end
   end
 
   def show
+    @tip = Tip.find(params[:id])
   end
+
+  private
+    def tip_params
+      params.require(:tip).permit(:desc, :odds, :stake, :info)
+    end
 end
