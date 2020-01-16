8c14e0d224095b7a6ed8f20a3c32eb4f7129e0fb
japoco <jacopo@airmail.cc>
2020-01-16 11:30:25 +0100

    schedule now has legible output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def schedule
-    @response = RestClient.get 'https://app.oddsapi.io/api/v1/odds?apikey=9a68c4a0-3629-11ea-8515-43fe73a2e3fb&sport=soccer'
+    require "json"
+    require "time"
+    @response = JSON.parse(RestClient.get('https://app.oddsapi.io/api/v1/odds?apikey=9a68c4a0-3629-11ea-8515-43fe73a2e3fb&sport=soccer'))
+    @response.each { |m| m['event']['start_time'] = Time.parse(m['event']['start_time']) }
+    @response.select! { |m| m['event']['start_time'] > Time.now }
+    @response.select! { |m| m['event']['start_time'] - Time.now < 60 * 60 * 24 * 2 }
+    @response.select! { |m| m['sites']['1x2'].key? 'bet365' }
+    @response.sort! { |x,y| x['event']['start_date'] <=> y['event']['start_date'] }
     render "/schedule"
   end
 
