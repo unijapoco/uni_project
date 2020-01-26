6495566e8216182b91cd1dcfacd3101eb78fce5e
japoco <jacopo@airmail.cc>
2020-01-27 00:08:43 +0100

    fixed post likes and comments in the views

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,13 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @on = @tip.likes.create(user: current_user)
+    @on = @post.likes.create(user: current_user)
+    redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @post.likes.find(params[:id]).destroy
+    redirect_to post_path(@post)
   end
 end
